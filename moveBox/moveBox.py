d9346b30f6d34c4efb75d3ead513043ad081a5c6
huangchengjian <xyyxhcj@qq.com>
2019-04-16 12:32:42 +0800

    run

--- moveBox/moveBox.py
+++ moveBox/moveBox.py
@@ -1,10 +1,19 @@
 # 配置
 # 空位
+import copy
+
 EMPTY = '.'
 # 3个方向移位时x,y坐标变更值
 NEXT = [(-1, 0), (1, 0), (0, 1)]
 # 使用栈遍历
 STACK = []
+
+
+# 更新字典
+def update(t_dict, y, x, value):
+    if y not in t_dict:
+        t_dict[y] = {}
+    t_dict[y][x] = value
 
 
 # 每一局的数据
@@ -31,32 +40,31 @@ class GameData:
         if self.try_solve():
             print('win')
 
+    def try_solve(self):
+        while len(STACK) > 0:
+            board = STACK.pop()
+            if self.is_win(board):
+                return True
+            if board.turn == self.max_turn:
+                continue
+            else:
+                board.run(self.max_turn)
+        return False
 
-def try_solve(self):
-    while len(STACK) > 0:
-        board = STACK.pop()
-        if self.is_win(board):
-            return True
-        if board.turn == self.max_turn:
-            continue
-        else:
-            board.run()
-
-
-def is_win(self, board):
-    for y in range(self.rows):
-        for char in board.data[y]:
-            if EMPTY != char:
-                return False
-    return True
+    def is_win(self, board):
+        for y in range(self.rows):
+            for char in board.data[y]:
+                if EMPTY != char:
+                    return False
+        return True
 
 
 # 每一步的盘面
 class Board:
     # 初始化盘面
-    def __init__(self, lines: []) -> None:
+    def __init__(self, lines: [], turn=0) -> None:
         # 记录步数
-        self.turn = 0
+        self.turn = turn
         # 行数 lines长度
         self.rows = len(lines)
         # 列数 line字符数
@@ -65,50 +73,74 @@ class Board:
         for y in range(self.rows):
             line = []
             self.data.append(line)
-            if self.columns == 0:
-                self.columns = len(lines[y])
             for x in range(len(lines[y])):
                 if '\n' != lines[y][x]:
                     line.append(lines[y][x])
+            if self.columns == 0:
+                self.columns = len(line)
 
     def in_area(self, x, y):
         return 0 <= x < self.columns and 0 <= y < self.rows
 
-    def swap(self, x, y, x_t, y_t):
+    def swap(self, data, x, y, x_t, y_t):
         if self.in_area(x, y) and self.in_area(x_t, y_t):
-            self.data[y][x], self.data[y_t][x_t] = self.data[y_t][x_t], self.data[y][x]
+            data[y][x], data[y_t][x_t] = data[y_t][x_t], data[y][x]
 
-    def run(self):
+    def run(self, max_turn):
         # 遍历每个箱子
         for y in range(self.rows):
             for x, char in enumerate(self.data[y]):
                 if EMPTY != char:
                     # 移动,仅处理箱子
-                    for i, move_x, move_y in range(len(NEXT)):
+                    for i, (move_x, move_y) in enumerate(NEXT):
                         x_t, y_t = x + move_x, y + move_y
                         # 与左右下交换,如果左方为箱子时不处理,
                         if i == 0 and EMPTY == self.data[y_t][x_t]:
                             continue
-                        self.swap(x, y, x_t, y_t)
+                        data = copy.deepcopy(self.data)
+                        self.swap(data, x, y, x_t, y_t)
                         while True:
                             # 掉落
-                            self.drop()
+                            self.drop(data)
                             # 标记+消除
-                            if not self.mark_clean():
-                                self.turn += 1
-                                STACK.append(self)
+                            if not self.mark_clean(data):
                                 break
+                        STACK.append(Board(data, self.turn + 1))
 
-    def drop(self):
-        y = self.rows
+    def drop(self, data):
+        y = self.rows - 1
         for x in range(self.columns):
             # 对最底一行逐列遍历,处理每列的箱子掉落
             i, curr = y, y
             while i > 0:
-                pass
+                if EMPTY != data[i][x]:
+                    if curr != i:
+                        data[i][x], data[curr][x] = data[curr][x], data[i][x]
+                    curr -= 1
+                i -= 1
 
-    def mark_clean(self):
-        pass
+    def mark_clean(self, data):
+        # 存储已标记坐标
+        marks = {}
+        clean = False
+        # 遍历每个箱子,向右方和下方各扫描两个箱子,如果重复则标记
+        for y in range(self.rows):
+            for x, char in enumerate(data[y]):
+                if EMPTY != char:
+                    for i in range(1, len(NEXT)):
+                        x2, y2 = x + NEXT[i][0], y + NEXT[i][1]
+                        x3, y3 = x2 + NEXT[i][0], y2 + NEXT[i][1]
+                        if self.in_area(x2, y2) and self.in_area(x3, y3) and data[y2][x2] == data[y3][x3] == data[y][x]:
+                            # 标记
+                            update(marks, y, x, True)
+                            update(marks, y2, x2, True)
+                            update(marks, y3, x3, True)
+                            clean = True
+        # 消除
+        for y, mark_y in marks.items():
+            for x in mark_y:
+                data[y][x] = EMPTY
+        return clean
 
     def print(self):
         for y in range(self.rows):
@@ -118,3 +150,4 @@ class Board:
 if __name__ == '__main__':
     game_data = GameData('boston_09.txt')
     game_data.start_board.print()
+    game_data.solve()
